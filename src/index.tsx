9f674fa55cd60bff43a323918173dd2af5ba4d32
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2022-12-27 12:17:51 -0500

    Revert "Chess ai"

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import Home from './Home'
+import paragraphs from './Paragraphs.json'
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-
 
 root.render(
     <React.StrictMode>
